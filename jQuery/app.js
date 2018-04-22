0254e47349121980c14a7b3a92871057ab82b89d
Sakina Shivji <shivjisakina@gmail.com>
2018-04-22 15:17:18 -0400

    adding jquery

---
+++ jQuery/app.js
@@ -0,0 +1,13 @@
+$(".red").click(function(){
+    //$(this).css("background-color", "black")
+    $(this).appendTo(".all");
+    //$(this).text("hello");
+})
+
+$(".blue").click(function(){
+    $(this).appendTo(".all");
+})
+
+$(".green").click(function(){
+    $(this).appendTo(".all");
+})
