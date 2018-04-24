10d177253da022a5c006838596ef3f6d99dac559
Sakina Shivji <shivjisakina@gmail.com>
2018-04-24 19:33:52 -0400

    added jquery homework

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,8 +1,15 @@
 $(".red").click(function(){
-    //$(this).css("background-color", "black")
+    $(this).css("background-color", "black")
     $(this).appendTo(".all");
-    //$(this).text("hello");
+    $(this).text("hello");
+    $(this).css("color", "skyblue")
+    $( ".red" ).animate({
+        top: "400px",
+        position: "absolute"
+      }, 5000);
+
 })
+
 
 $(".blue").click(function(){
     $(this).appendTo(".all");
